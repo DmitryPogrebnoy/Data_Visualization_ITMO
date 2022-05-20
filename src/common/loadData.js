36a1833dd2c63a54280cab1ed5001ec3ce4ba5bd
Dmitry Pogrebnoy <pogrebnoy.inc@gmail.com>
2022-05-21 00:29:19 +0700

    Load csv using d3

--- src/common/loadData.js
+++ src/common/loadData.js
@@ -5,13 +5,9 @@ async function loadData() {
         return memoizedData;
     }
     // Data from notebook
-    const dataText = await fetch(
-        "https://raw.githubusercontent.com/DmitryPogrebnoy/Data_Visualization_ITMO/master/resources/augmented_covid_19.csv"
-    ).then((response) => {
-        return response.text();
-    });
+    memoizedData = await d3.csv(
+        "https://raw.githubusercontent.com/DmitryPogrebnoy/Data_Visualization_ITMO/master/resources/augmented_covid_19.csv");
 
-    memoizedData = d3.csvParse(dataText, d3.autoType);
     return memoizedData
 }
 
